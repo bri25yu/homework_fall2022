d44ac027f409bbd258d60374a16dcf940ce8db76
bri25yu <bri25yu@gmail.com>
2022-08-31 17:46:05 -0700

    implementing MLPPolicySL, get_action, and forward

--- hw1/cs285/policies/MLP_policy.py
+++ hw1/cs285/policies/MLP_policy.py
@@ -80,8 +80,17 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
         else:
             observation = obs[None]
 
+        # Convert our obs into a form usable by our model
+        obs_pt = ptu.from_numpy(observation)
+
+        # Run our model
+        action_pt = self(obs_pt)
+
+        # Convert our output action into a form usable by downstream
+        action = ptu.to_numpy(action_pt)
+
         # TODO return the action that the policy prescribes
-        raise NotImplementedError
+        return action
 
     # update/train this policy
     def update(self, observations, actions, **kwargs):
@@ -93,7 +102,10 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
     # return more flexible objects, such as a
     # `torch.distributions.Distribution` object. It's up to you!
     def forward(self, observation: torch.FloatTensor) -> Any:
-        raise NotImplementedError
+        if self.discrete:
+            return self.logits_na(observation)
+        else:
+            return self.mean_net(observation)
 
 
 #####################################################
@@ -109,7 +121,30 @@ class MLPPolicySL(MLPPolicy):
             adv_n=None, acs_labels_na=None, qvals=None
     ):
         # TODO: update the policy and return the loss
-        loss = TODO
+
+        # Retrieve relevant objects from self
+        loss_fn = self.loss
+        optimizer = self.optimizer
+
+        # Setup our optimizer for this train step
+        optimizer.zero_grad()
+
+        # Convert our obs into a form usable by our model
+        obs_pt = ptu.from_numpy(observations)
+
+        # Retrieve model output actions
+        model_actions = self(obs_pt)
+
+        # Convert loss inputs to a form usable by the loss object
+        actions_pt = ptu.from_numpy(actions)
+
+        # Calculate loss
+        loss = loss_fn(model_actions, actions_pt)
+
+        # Update parameters
+        loss.backward()
+        optimizer.step()
+
         return {
             # You can add extra logging information here, but keep this line
             'Training Loss': ptu.to_numpy(loss),
