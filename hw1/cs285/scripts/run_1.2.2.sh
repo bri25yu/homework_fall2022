19ff1bcad84c5010d167107da96f0f3626c5ee26
bri25yu <bri25yu@gmail.com>
2022-09-04 16:02:54 -0700

    improving walker params

--- hw1/cs285/scripts/run_1.2.2.sh
+++ hw1/cs285/scripts/run_1.2.2.sh
@@ -1,48 +1,31 @@
 exp_name="HalfCheetah"
-n_layers=2
-size=64
 python cs285/scripts/run_hw1.py \
     --expert_policy_file cs285/policies/experts/$exp_name.pkl \
     --env_name $exp_name-v4 \
     --exp_name bc_$exp_name \
-    --n_iter 1 \
-    --n_layers $n_layers \
-    --size $size \
     --expert_data cs285/expert_data/expert_data_$exp_name-v4.pkl \
     --video_log_freq -1
 
 # Best is 1732 at 2/64/10000/L1Loss over 1e-3, 2e-3, 3e-3 at 2e-3
 exp_name="Hopper"
-n_layers=2
-size=64
 python cs285/scripts/run_hw1.py \
     --expert_policy_file cs285/policies/experts/$exp_name.pkl \
     --env_name $exp_name-v4 \
     --exp_name bc_$exp_name \
-    --n_iter 1 \
-    --n_layers $n_layers \
-    --size $size \
     --num_agent_train_steps_per_iter 10000 \
     -lr 2e-3 \
     --loss L1Loss \
     --expert_data cs285/expert_data/expert_data_$exp_name-v4.pkl \
     --video_log_freq -1
 
-# Best is 314 at 2/1024/10000/MSELoss over 1e-5, 2e-5, 3e-5 at 3e-5
+# Best is 5272 at 2/64/10000/L1Loss over 1e-3, 2e-3, 3e-3 at 1e-3
 exp_name="Walker2d"
-n_layers=2
-size=1024
 python cs285/scripts/run_hw1.py \
     --expert_policy_file cs285/policies/experts/$exp_name.pkl \
     --env_name $exp_name-v4 \
     --exp_name bc_$exp_name \
-    --n_iter 1 \
-    --n_layers $n_layers \
-    --size $size \
     --num_agent_train_steps_per_iter 10000 \
-    -lr 1e-5 \
-    --loss HuberLoss \
-    --batch_size 10000000 \
-    --train_batch_size 10000 \
+    -lr 1e-3 \
+    --loss L1Loss \
     --expert_data cs285/expert_data/expert_data_$exp_name-v4.pkl \
     --video_log_freq -1
