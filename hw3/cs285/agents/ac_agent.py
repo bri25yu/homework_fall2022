8e8fad311450cb80de5be6b88fd042afd177ec40
bri25yu <bri25yu@gmail.com>
2022-10-07 18:45:09 -0700

    implementing ac agent train and estimate advantage

--- hw3/cs285/agents/ac_agent.py
+++ hw3/cs285/agents/ac_agent.py
@@ -31,32 +31,54 @@ class ACAgent(BaseAgent):
         self.replay_buffer = ReplayBuffer()
 
     def train(self, ob_no, ac_na, re_n, next_ob_no, terminal_n):
+        # Retrieve relevant objects from self
+        num_critic_updates_per_agent_update = self.agent_params['num_critic_updates_per_agent_update']
+        num_actor_updates_per_agent_update = self.agent_params['num_actor_updates_per_agent_update']
+        critic = self.critic
+        actor = self.actor
+
         # TODO Implement the following pseudocode:
         # for agent_params['num_critic_updates_per_agent_update'] steps,
         #     update the critic
+        critic_loss = None
+        for _ in range(num_critic_updates_per_agent_update):
+            loss = critic.update(ob_no, ac_na, next_ob_no, re_n, terminal_n)
 
         # advantage = estimate_advantage(...)
+        advantage = self.estimate_advantage(ob_no, next_ob_no, re_n, terminal_n)
 
         # for agent_params['num_actor_updates_per_agent_update'] steps,
         #     update the actor
+        actor_loss = None
+        for _ in range(num_actor_updates_per_agent_update):
+            actor_loss = actor.update(ob_no, ac_na, advantage)
 
         loss = OrderedDict()
-        loss['Critic_Loss'] = TODO
-        loss['Actor_Loss'] = TODO
+        loss['Critic_Loss'] = critic_loss
+        loss['Actor_Loss'] = actor_loss
 
         return loss
 
     def estimate_advantage(self, ob_no, next_ob_no, re_n, terminal_n):
+        # Retrieve relevant objects from self
+        critic = self.critic
+        gamma = self.gamma
+        standardize_advantages = self.standardize_advantages
+
         # TODO Implement the following pseudocode:
         # 1) query the critic with ob_no, to get V(s)
         # 2) query the critic with next_ob_no, to get V(s')
         # 3) estimate the Q value as Q(s, a) = r(s, a) + gamma*V(s')
         # HINT: Remember to cut off the V(s') term (ie set it to 0) at terminal states (ie terminal_n=1)
         # 4) calculate advantage (adv_n) as A(s, a) = Q(s, a) - V(s)
-        adv_n = TODO
+        V_s = critic.forward_np(ob_no)
+        V_s_prime = critic.forward_np(next_ob_no)
+        Q_s_a = re_n + gamma * V_s_prime * (1 - terminal_n)
+        adv_n = Q_s_a - V_s
 
-        if self.standardize_advantages:
+        if standardize_advantages:
             adv_n = (adv_n - np.mean(adv_n)) / (np.std(adv_n) + 1e-8)
+
         return adv_n
 
     def add_to_replay_buffer(self, paths):
