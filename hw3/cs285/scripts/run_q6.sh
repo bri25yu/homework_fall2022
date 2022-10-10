bc73046a599cc8356e03664751c615c7ff2e410f
bri25yu <bri25yu@gmail.com>
2022-10-10 13:21:15 -0700

    adding actual run q6 script; adding more info to run q6 and q6 test scripts

---
+++ hw3/cs285/scripts/run_q6.sh
@@ -0,0 +1,16 @@
+# After 10000 steps, return should be above -40 (trending toward positive)
+# After 50000 steps, return should be around 200
+# Target 300 average return under 200000 steps
+python cs285/scripts/run_hw3_sac.py \
+    --env_name HalfCheetah-v4 --ep_len 150 \
+    --exp_name q6b_sac_HalfCheetah \
+    --discount 0.99 \
+    --scalar_log_freq 1500 \
+    -n 2000000 \
+    -l 2 \
+    -s 256 \
+    -b 1500 \
+    -eb 1500 \
+    -lr 3e-4 \
+    --init_temperature 0.1 \
+    --seed 1
--- hw3/cs285/scripts/run_q6_test.sh
+++ hw3/cs285/scripts/run_q6_test.sh
@@ -1,3 +1,6 @@
+# After 10000 steps, return should be near or above 100
+# After 20000 steps, return should reach 1000
+# Target 1000 eval average return under 100000 steps
 python cs285/scripts/run_hw3_sac.py \
     --env_name InvertedPendulum-v4 \
     ----exp_name q6a_sac_InvertedPendulum \
