89138f6e8d0d23edc860dff294a3274815ed087c
bri25yu <bri25yu@gmail.com>
2022-10-25 22:50:53 -0700

    fixing continuous action dist; adding pg cartpole exp

--- rl/experiments/policy_gradient.py
+++ rl/experiments/policy_gradient.py
@@ -13,13 +13,26 @@ class PolicyGradientExperimentBase(OffPolicyTrainingPipelineBase):
 
 
 class PolicyGradientInvertedPendulumExperiment(PolicyGradientExperimentBase):
-    TRAIN_STEPS = 1000
-    EVAL_STEPS = 10
-    LEARNING_RATE = 3e-4
-    TRAIN_BATCH_SIZE = 1
+    TRAIN_STEPS = 100
+    EVAL_STEPS = 1
+    LEARNING_RATE = 5e-3
+    TRAIN_BATCH_SIZE = 10
 
     def get_env(self) -> Tuple[Env, EnvironmentInfo]:
         env = make("InvertedPendulum-v4")
+        environment_info = EnvironmentInfo.from_env(env)
+
+        return env, environment_info
+
+
+class PolicyGradientCartpoleExperiment(PolicyGradientExperimentBase):
+    TRAIN_STEPS = 100
+    EVAL_STEPS = 1
+    LEARNING_RATE = 5e-3
+    TRAIN_BATCH_SIZE = 1
+
+    def get_env(self) -> Tuple[Env, EnvironmentInfo]:
+        env = make("CartPole-v1")
         environment_info = EnvironmentInfo.from_env(env)
 
         return env, environment_info
--- rl/infrastructure/policy.py
+++ rl/infrastructure/policy.py
@@ -1,6 +1,6 @@
 import torch
-from torch.nn import Module
-from torch.distributions import AffineTransform, ComposeTransform, Normal, TanhTransform
+from torch.nn import Module, Parameter
+from torch.distributions import AffineTransform, Normal, TanhTransform, TransformedDistribution
 
 from rl.infrastructure.environment_info import EnvironmentInfo
 from rl.infrastructure.model_output import ModelOutput
@@ -16,18 +16,23 @@ class PolicyBase(Module):
         self.environment_info = environment_info
 
         action_range_low, action_range_high = self.environment_info.action_range
-        action_range_scale = (action_range_high - action_range_low) / 2
         action_range_loc = (action_range_high + action_range_low) / 2
-        action_range_transform = AffineTransform(loc=action_range_loc, scale=action_range_scale)
+        action_range_scale = (action_range_high - action_range_low) / 2
 
-        self.action_transform = ComposeTransform([TanhTransform(), action_range_transform])
+        torch_parameter_from_np = lambda v: Parameter(torch.from_numpy(v), requires_grad=False)
+        self.action_range_loc = torch_parameter_from_np(action_range_loc)
+        self.action_range_scale = torch_parameter_from_np(action_range_scale)
+
+        action_range_transform = AffineTransform(loc=self.action_range_loc, scale=self.action_range_scale)
+
+        self.action_transforms = [TanhTransform(), action_range_transform]
 
     def forward(self, trajectory: Trajectory) -> ModelOutput:
         raise NotImplementedError
 
     def create_continuous_actions_distribution(self, loc: torch.Tensor, log_scale: torch.Tensor) -> torch.distributions.Distribution:
         log_scale_min, log_scale_max = self.LOG_SCALE_BOUNDS
-        L = self.environment_info.L
+        L = loc.size()[0]
         action_shape = self.environment_info.action_shape
 
         log_scale_clipped = log_scale.clamp(min=log_scale_min, max=log_scale_max)
@@ -36,6 +41,6 @@ class PolicyBase(Module):
 
         actions_dist_unscaled = Normal(loc, scale)
 
-        action_dist = self.action_transform(actions_dist_unscaled)
+        action_dist = TransformedDistribution(actions_dist_unscaled, self.action_transforms)
 
         return action_dist
