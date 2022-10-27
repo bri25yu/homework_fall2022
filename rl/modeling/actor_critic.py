ef6550840d0e7d31bb205899653a19a5d6b52201
bri25yu <bri25yu@gmail.com>
2022-10-26 19:18:19 -0700

    trying to fix actor critic but temporarily giving up lol

--- rl/modeling/actor_critic.py
+++ rl/modeling/actor_critic.py
@@ -55,17 +55,22 @@ class ActorCriticBase(PolicyBase):
             .sum(dim=-1, keepdim=True)
 
         V_s = self.critic(trajectories.observations)
-        V_s_prime = self.critic(trajectories.next_observations).detach()
-        Q_s_a = trajectories.rewards + gamma * V_s_prime * (1 - trajectories.terminals)
-        advantages = self._normalize(Q_s_a - V_s)
 
-        policy_loss = (-action_log_probs * (advantages.detach())).sum()
-        critic_loss = self.critic_loss_fn(V_s, Q_s_a)
+        with torch.no_grad():
+            rewards_normed = self._normalize(trajectories.rewards)
+            V_s_prime = self.critic(trajectories.next_observations)
+            Q_s_a = rewards_normed + gamma * V_s_prime * (~trajectories.terminals)
+            advantages = self._normalize(Q_s_a - V_s)
+
+        policy_loss = (-action_log_probs * advantages).sum()
+        critic_loss = self.critic_loss_fn(V_s, advantages)
 
         total_loss = policy_loss + critic_loss
 
         def check_shapes():
             assert action_log_probs.size() == (L, 1)
+            assert V_s.size() == (L, 1)
+            assert Q_s_a.size() == (L, 1)
             assert advantages.size() == (L, 1)
 
         check_shapes()
