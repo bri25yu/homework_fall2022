7941c1c3f45d0227663cde20610d881141e99907
bri25yu <bri25yu@gmail.com>
2022-10-29 17:47:46 -0700

    adding contrastive v2 with repeats

---
+++ rl/experiments/contrastive_v2_with_repeats.py
@@ -0,0 +1,32 @@
+from gym import Env, make
+
+from rl.modeling.contrastive_v2 import ContrastiveV2Base
+from rl.training_pipelines import OffPolicyWithRepeatsTrainingPipelineBase
+from rl.infrastructure import PolicyBase
+
+
+class ContrastiveV2WithRepeatsExperimentBase(OffPolicyWithRepeatsTrainingPipelineBase):
+    NUM_STEPS_PER_SAMPLE = 5
+
+    def get_policy(self, env: Env) -> PolicyBase:
+        return ContrastiveV2Base(env=env, gamma=0.99)
+
+
+class ContrastiveV2WithRepeatsCartPoleExperiment(ContrastiveV2WithRepeatsExperimentBase):
+    TRAIN_STEPS = 100
+    EVAL_STEPS = 1
+    LEARNING_RATE = 5e-3
+    TRAIN_BATCH_SIZE = 1
+
+    def get_env(self) -> Env:
+        return make("CartPole-v1")
+
+
+class ContrastiveV2WithRepeatsInvertedPendulumExperiment(ContrastiveV2WithRepeatsExperimentBase):
+    TRAIN_STEPS = 100
+    EVAL_STEPS = 1
+    LEARNING_RATE = 5e-3
+    TRAIN_BATCH_SIZE = 1
+
+    def get_env(self) -> Env:
+        return make("InvertedPendulum-v4")
