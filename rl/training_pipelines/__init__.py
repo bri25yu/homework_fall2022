c6f6e23ea57a7b6b68d1c0e49756399b631bb0ca
bri25yu <bri25yu@gmail.com>
2022-11-03 23:30:29 -0700

    removing contrastive v2 and with repeats exps

--- rl/training_pipelines/__init__.py
+++ rl/training_pipelines/__init__.py
@@ -1,8 +1,6 @@
 from rl.training_pipelines.off_policy import OffPolicyTrainingPipelineBase
-from rl.training_pipelines.off_policy_with_repeats import OffPolicyWithRepeatsTrainingPipelineBase
 
 
 __all__ = [
     "OffPolicyTrainingPipelineBase",
-    "OffPolicyWithRepeatsTrainingPipelineBase",
 ]
