d1ec1cb8c1e8ccef60bf7c30f1bece56986682fe
bri25yu <bri25yu@gmail.com>
2022-11-21 11:19:28 -0800

    adding additional runs for q1

--- hw5/cs285/scripts/run_q1.1.sh
+++ hw5/cs285/scripts/run_q1.1.sh
@@ -11,3 +11,9 @@ python cs285/scripts/run_hw5_expl.py --env_name $env --use_rnd \
 --unsupervised_exploration --exp_name q1_env2_rnd
 python cs285/scripts/run_hw5_expl.py --env_name $env \
 --unsupervised_exploration --exp_name q1_env2_random
+
+env="PointmassHard-v0"
+python cs285/scripts/run_hw5_expl.py --env_name $env --use_rnd \
+--unsupervised_exploration --exp_name q1_env2_rnd
+python cs285/scripts/run_hw5_expl.py --env_name $env \
+--unsupervised_exploration --exp_name q1_env2_random
--- hw5/cs285/scripts/run_q1.2.sh
+++ hw5/cs285/scripts/run_q1.2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+python cs285/scripts/run_hw5_expl.py --env_name PointmassEasy-v0 \
+--unsupervised_exploration --use_custom_exploration --exp_name q1_alg_med
 python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 \
 --unsupervised_exploration --use_custom_exploration --exp_name q1_alg_med
 python cs285/scripts/run_hw5_expl.py --env_name PointmassHard-v0 \
