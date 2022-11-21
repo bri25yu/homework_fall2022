0d9c993f83ff49139adbea4929c0fd6bca139fc6
bri25yu <bri25yu@gmail.com>
2022-11-20 19:41:18 -0800

    fixing question numbering; fixing exploit reward shift

---
+++ hw5/cs285/scripts/run_q2.2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+env="PointmassMedium-v0"
+
+expl_step_values=(5000 15000)
+
+for expl_step_value in ${expl_step_values[*]}
+do
+    python cs285/scripts/run_hw5_expl.py --env_name $env --use_rnd \
+    --num_exploration_steps="$expl_step_value" --offline_exploitation --cql_alpha=0.1 \
+    --unsupervised_exploration --exp_name q2_cql_numsteps_"$expl_step_value"
+
+    python cs285/scripts/run_hw5_expl.py --env_name $env --use_rnd \
+    --num_exploration_steps="$expl_step_value" --offline_exploitation --cql_alpha=0.0 \
+    --unsupervised_exploration --exp_name q2_dqn_numsteps_"$expl_step_value"
+done
