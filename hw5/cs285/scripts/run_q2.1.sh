dc7d1aca5682952a312da8ac7af0285a5223fff4
bri25yu <bri25yu@gmail.com>
2022-11-20 20:11:41 -0800

    using reward shifting and scaling; adding scripts for q2.3 and 3

--- hw5/cs285/scripts/run_q2.1.sh
+++ hw5/cs285/scripts/run_q2.1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --exp_name q2_dqn \
---use_rnd --unsupervised_exploration --offline_exploitation --cql_alpha=0
+--use_rnd --unsupervised_exploration --exploit_rew_shift 1 --exploit_rew_scale 100 --offline_exploitation --cql_alpha=0
 
 python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --exp_name q2_cql \
---use_rnd --unsupervised_exploration --offline_exploitation --cql_alpha=0.1
+--use_rnd --unsupervised_exploration --exploit_rew_shift 1 --exploit_rew_scale 100 --offline_exploitation --cql_alpha=0.1
---
+++ hw5/cs285/scripts/run_q2.3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+env="PointmassMedium-v0"
+
+cql_alphas=(0.02 0.5)
+
+for cql_alpha in ${cql_alphas[*]}
+do
+    python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --use_rnd \
+    --unsupervised_exploration --exploit_rew_shift 1 --exploit_rew_scale 100 --offline_exploitation --cql_alpha="$cql_alpha" \
+    --exp_name q2_alpha"$cql_alpha"
+done
