c3fbf13a56d29bb539ed55b57c3659abd8a0bef8
bri25yu <bri25yu@gmail.com>
2022-11-21 11:44:29 -0800

    adding dqn scaled exps

--- hw5/cs285/scripts/run_q2.1.sh
+++ hw5/cs285/scripts/run_q2.1.sh
@@ -5,3 +5,6 @@ python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --exp_name q2
 
 python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --exp_name q2_cql \
 --use_rnd --unsupervised_exploration --offline_exploitation --cql_alpha=0.1
+
+python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --exp_name q2_dqn_scaled \
+--use_rnd --unsupervised_exploration --offline_exploitation --exploit_rew_shift 1 --exploit_rew_scale 100 --cql_alpha=0
--- hw5/cs285/scripts/run_q3.sh
+++ hw5/cs285/scripts/run_q3.sh
@@ -3,9 +3,13 @@
 python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --use_rnd \
 --num_exploration_steps=20000 --cql_alpha=0.0 --exp_name q3_medium_dqn
 python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --use_rnd \
+--num_exploration_steps=20000 --cql_alpha=0.0 --exploit_rew_shift 1 --exploit_rew_scale 100 --exp_name q3_medium_dqn_scaled
+python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --use_rnd \
 --num_exploration_steps=20000 --cql_alpha=1.0 --exp_name q3_medium_cql
 
 python cs285/scripts/run_hw5_expl.py --env_name PointmassHard-v0 --use_rnd \
 --num_exploration_steps=20000 --cql_alpha=0.0 --exp_name q3_hard_dqn
+python cs285/scripts/run_hw5_expl.py --env_name PointmassHard-v0 --use_rnd \
+--num_exploration_steps=20000 --cql_alpha=0.0 --exploit_rew_shift 1 --exploit_rew_scale 100 --exp_name q3_hard_dqn_scaled
 python cs285/scripts/run_hw5_expl.py --env_name PointmassHard-v0 --use_rnd \
 --num_exploration_steps=20000 --cql_alpha=1.0 --exp_name q3_hard_cql
