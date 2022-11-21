be64eb3ced40fd36d69a86a2e50d5675ff50f998
bri25yu <bri25yu@gmail.com>
2022-11-20 18:38:25 -0800

    adding q2 and a3 run scripts

--- hw5/cs285/scripts/run_q1_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-env="PointmassEasy-v0"
-
-python cs285/scripts/run_hw5_expl.py --env_name $env --use_rnd \
---unsupervised_exploration --exp_name test_q1_rnd
-
-python cs285/scripts/run_hw5_expl.py --env_name $env \
---unsupervised_exploration --exp_name test_q1_random
---
+++ hw5/cs285/scripts/run_q2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 \
+--unsupervised_exploration --use_custom_exploration --exp_name q1_alg_med
+python cs285/scripts/run_hw5_expl.py --env_name PointmassHard-v0 \
+--unsupervised_exploration --use_custom_exploration --exp_name q1_alg_hard
