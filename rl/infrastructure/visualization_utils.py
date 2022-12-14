38923635a206a824aca8869560324839c4c07355
bri25yu <bri25yu@gmail.com>
2022-12-13 20:45:31 -0800

    improving vis utils

--- rl/infrastructure/visualization_utils.py
+++ rl/infrastructure/visualization_utils.py
@@ -1,15 +1,15 @@
-from typing import Any, List, Tuple
+from typing import Any, Dict, List, Tuple
 
-import os
+from os.path import join
 
 from dataclasses import dataclass
 
 import tensorflow as tf
 tf.get_logger().setLevel("ERROR")
 
-from tensorflow.python.summary.summary_iterator import summary_iterator
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
-import numpy as np
+from numpy import array, ndarray
 
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
@@ -21,69 +21,28 @@ from rl import RESULTS_DIR
 __all__ = ["create_graph", "create_comparative_graph"]
 
 
-def load_eventfile(log_dir: str) -> List:
-    # Get the eventfile_path
-    eventfile_name = os.listdir(log_dir)[0]
-    eventfile_path = os.path.join(log_dir, eventfile_name)
+def get_summary(experiment_dir: str) -> Tuple[List[float], Dict[str, List[float]]]:
+    event_accumulator = EventAccumulator(join(RESULTS_DIR, experiment_dir))
+    event_accumulator.Reload()
 
-    return list(summary_iterator(eventfile_path))
+    tags = event_accumulator.Tags()
 
+    steps = None
+    data = dict()
+    for value_name in tags["scalars"]:
+        if steps is None:
+            scalars = event_accumulator.Scalars(value_name)
+            steps = [s.step for s in scalars]
 
-def filter_summaries_by_tag(summaries: List, tag: str) -> List[Tuple]:
-    """
-    Filters summaries for all events 
-    """
-    value_is_tag = lambda v: v.tag == tag
-    get_value_tag_from_event = lambda e: next(filter(value_is_tag, e.summary.value), None)
+        scalars = event_accumulator.Scalars(value_name)
+        data[value_name] = [s.value for s in scalars]
 
-    filtered = []
-    for event in summaries:
-        value = get_value_tag_from_event(event)
-        if value is None:
-            continue
-
-        filtered.append((event, value))
-
-    return filtered
-
-
-def get_first_simple_value(summaries: List[Tuple]) -> float:
-    """
-    Takes in the output of `filter_summaries_by_tag`
-    """
-    return next(iter(summaries))[1].simple_value
-
-
-def get_first_tag_simple_value(summaries: List, tag: str) -> float:
-    filtered = filter_summaries_by_tag(summaries, tag)
-    return get_first_simple_value(filtered)
-
-
-def get_property_and_steps(log_dir: str, property_name: str) -> Tuple[List[float], List[float]]:
-    """
-    Returns a tuple of steps and property values.
-
-    The arrays are sorted ascending in steps.
-    """
-    experiment_summary = load_eventfile(log_dir)
-
-    train_returns = filter_summaries_by_tag(experiment_summary, property_name)
-    steps = [r[0].step for r in train_returns]
-    returns = [r[1].simple_value for r in train_returns]
-
-    steps = np.array(steps)
-    returns = np.array(returns)
-
-    sorted_idxs = steps.argsort()
-
-    steps = steps[sorted_idxs]
-    returns = returns[sorted_idxs]
-
-    return steps, returns
+    return steps, data
 
 
 def get_eval_returns(log_dir: str) -> Tuple[List[float], List[float]]:
-    return get_property_and_steps(log_dir, "return_eval")
+    steps, summary = get_summary(log_dir)
+    return steps, summary("return_eval")
 
 
 @dataclass
@@ -126,7 +85,7 @@ def plot_single_benchmark(ax: Axes, benchmark: BenchmarkVisualizationInfo) -> No
 
         data.append(returns)
 
-    data = np.array(data)
+    data = array(data)
     stds = data.std(axis=0)
     means = data.mean(axis=0)
 
@@ -160,7 +119,7 @@ def create_graph(experiment: Any) -> None:
     ax.set_title(f"Performance of {benchmark.display_name} on {benchmark.env_name} averaged over {len(benchmark.log_dirs)} seeds")
 
     finalize_graph(fig, ax, benchmark)
-    fig.savefig(os.path.join(benchmark.experiment.experiment_results_dir, "benchmark.png"))
+    fig.savefig(join(benchmark.experiment.experiment_results_dir, "benchmark.png"))
 
 
 def create_comparative_graph(experiments: List[Any]) -> None:
@@ -175,4 +134,4 @@ def create_comparative_graph(experiments: List[Any]) -> None:
     ax.set_title(f"Comparison of {','.join(experiment_names)} on {benchmark.env_name}")
 
     finalize_graph(fig, ax, benchmark)
-    fig.savefig(os.path.join(RESULTS_DIR, f"{benchmark.env_name}_{'_'.join(experiment_names)}.png"))
+    fig.savefig(join(RESULTS_DIR, f"{benchmark.env_name}_{'_'.join(experiment_names)}.png"))
